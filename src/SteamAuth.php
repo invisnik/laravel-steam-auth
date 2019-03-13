21a236b02914468cb069d6ded9c638cb9757fa8a
Grand Silence <mixtape774@gmail.com>
2019-03-14 02:47:49 +0300

    style refactor

--- src/SteamAuth.php
+++ src/SteamAuth.php
@@ -210,12 +210,11 @@ class SteamAuth implements SteamAuthInterface
         }
 
         $realm = Config::get('steam-auth.realm', $this->request->server('HTTP_HOST'));
-        
         $params = [
             'openid.ns'         => self::OPENID_NS,
             'openid.mode'       => 'checkid_setup',
             'openid.return_to'  => $return,
-            'openid.realm'      => (Config::get('steam-auth.https') ? 'https' : 'http').'://'. $realm,
+            'openid.realm'      => (Config::get('steam-auth.https') ? 'https' : 'http').'://'.$realm,
             'openid.identity'   => 'http://specs.openid.net/auth/2.0/identifier_select',
             'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
         ];
